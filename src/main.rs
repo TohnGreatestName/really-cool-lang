a5f874400bea7204f7d9b47fb5c562590e1728ab
TohnGreatestName <144180804+TohnGreatestName@users.noreply.github.com>
2024-02-23 01:57:46 +0000

    basic lexer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+use syntax::lexer::LexerError;
+
+use crate::syntax::lexer::LexerStream;
+
 mod syntax;
 
-fn main() {
+fn main() -> std::result::Result<(), LexerError> {
+    let mut v = LexerStream::new("abed".chars().enumerate());
+
+    v.advance(Some('a'))?;
+    v.advance(Some('b'))?;
+    v.advance(Some('c'))?;
+    v.advance(Some('d'))?;
     println!("Hello, world!");
+    Ok(())
 }
--- src/syntax/lexer.rs
+++ src/syntax/lexer.rs
@@ -1,11 +1,86 @@
-use std::str::Chars;
+use std::{fmt::Display, iter::Enumerate, str::Chars};
+
+use thiserror::Error;
 
 pub enum LexerType {
     UntilEof,
     UntilEnd(char),
 }
 
+type IndexedCharIter<'a> = Enumerate<Chars<'a>>;
+
 pub struct LexerStream<'a> {
-    chars: Chars<'a>,
+    chars: IndexedCharIter<'a>,
+    peek: Option<(usize, char)>,
 }
 
+impl<'a> LexerStream<'a> {
+    pub fn new(mut chars: IndexedCharIter<'a>) -> Self {
+        let peek = chars.next();
+        Self { chars, peek }
+    }
+
+    pub fn peek(&self, comparison: Option<char>) -> LexerResult<char> {
+        if let Some(comparison) = comparison {
+            if self.peek.map(|v| v.1) != Some(comparison) {
+                return Err(LexerError::incorrect_char(self.peek, comparison));
+            }
+        }
+
+        if let Some(current) = self.peek {
+            Ok(current.1)
+        } else {
+            Err(LexerError::eof())
+        }
+    }
+
+    pub fn advance(&mut self, comparison: Option<char>) -> LexerResult<char> {
+        let c = self.peek(comparison)?;
+        self.peek = self.chars.next();
+        Ok(c)
+    }
+}
+
+pub type LexerResult<T> = std::result::Result<T, LexerError>;
+
+#[derive(Debug, Error)]
+pub struct LexerError {
+    err: LexerErrorType,
+    position: usize,
+}
+
+impl LexerError {
+    pub fn eof() -> Self {
+        Self {
+            err: LexerErrorType::EOF,
+            position: 0, // TODO - "Enumerate" iterator helper does not let us access the current position without
+                         // advancing the iterator. Will fix this later
+        }
+    }
+
+    pub fn incorrect_char(got: Option<(usize, char)>, expected: char) -> Self {
+        let position = if let Some((idx, _)) = got {
+            idx
+        } else {
+            0 // TODO - position tracking in the stream if we encounter EOF
+        };
+        Self {
+            err: LexerErrorType::IncorrectChar(got.map(|v| v.1), expected),
+            position,
+        }
+    }
+}
+
+impl Display for LexerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "@[{}]: {}", self.position, self.err)
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum LexerErrorType {
+    #[error("got {0:?} but expected {1}")]
+    IncorrectChar(Option<char>, char),
+    #[error("encountered EOF")]
+    EOF,
+}
