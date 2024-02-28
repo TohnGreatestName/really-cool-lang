31bfc68d763b71d0497a0b088764bada5aa71857
TohnGreatestName <144180804+TohnGreatestName@users.noreply.github.com>
2024-02-29 10:07:11 +1300

    Update main.rs to local

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
-use syntax::lexer::LexerError;
+use syntax::lexer::{matchers::{AnyChar, NumericChar, SpecificChar}, LexerError};
 
 use crate::syntax::lexer::LexerStream;
 
 mod syntax;
 
 fn main() -> std::result::Result<(), LexerError> {
-    let mut v = LexerStream::new("(abcd)(bcda)".chars().enumerate());
+    let mut v = LexerStream::new("(1234)(2345)(22)".chars().enumerate());
 
-    while v.advance(Some('(')).is_ok() {
-        let mut second_stream = v.eat_until(')')?;
+    while v.advance::<SpecificChar<'('>>().is_ok() {
+        let mut second_stream = v.eat_until::<SpecificChar<')'>>().unwrap();
 
-        while let Ok(v) = second_stream.advance(None) {
-            println!("value: {:?}", v);
+
+        let mut chars = String::new();
+        loop {
+            match second_stream.advance::<NumericChar>() {
+                Ok(c) => chars.push(c),
+                Err(e) if e.is_eof() => break,
+                Err(e) => return Err(e)
+            }
         }
+        println!("{}", chars);
+
     }
 
+    println!("Hello WOrld");
     //println!("Hello, world! {:?}", v.peek(None)?);
     Ok(())
 }
